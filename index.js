faafff9ea4d4bbfe57a85748039f01fb7b525360
Admin <ivan_kruk@ukr.net>
2016-11-24 16:20:00 +0200

    update  first  project 241116

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+/* $(document).ready(function () {
   $('header').hide().slideDown(3000);
 });
 
 $(document).ready(function () {
   $('.slider').hide().slideDown(2500);
-});
+}); */
 
 $(function () {
   var $headerNavLi = $('.header-nav li').has('ul');
@@ -12,4 +12,23 @@ $(function () {
     e.preventDefault();
     $(this).closest('li').toggleClass('opened');
   });
+});
+
+
+$(document).ready(function() {
+
+  $("#buy_price").slider({
+    range: true,
+    min: 500, // минимальное значение цены
+    max: 120000, // максимальное значение цены
+    step: 1500, // шаг слайдера
+    values: [ 1500, 100000],  // начальные значения - позиции ползунков на шкале
+
+    slide: function( event, ui ) {
+      $( "input[name=price_s]" ).val(  ui.values[ 0 ] ); // выводим  значение от
+      $( "input[name=price_f]" ).val(  ui.values[ 1 ] ); // выводим  значение до
+    },
+    stop: function(event, ui) { show(); } // выполняем действие  после остановки ползунка, в нашем случае функция show
+  });
+
 });
