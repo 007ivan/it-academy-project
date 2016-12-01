d714bb3f4d3d2e10482daf12070b114ec82560cc
Admin <ivan_kruk@ukr.net>
2016-12-01 15:36:06 +0200

    update  first  project with 011216

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,8 @@
  $('.slider').hide().slideDown(2500);
  }); */
 
+/* menu */
+
 $(function () {
   var $headerNavLi = $('.header-nav li').has('ul');
   $headerNavLi.find('a').on('click', function (e) {
@@ -13,70 +15,63 @@ $(function () {
     $(this).closest('li').toggleClass('opened');
   });
 });
+/* slider product-grid*/
+
 
 $("#slider").slider({
   min: 0,
   max: 1000,
-  values: [0,1000],
+  values: [0, 1000],
   range: true,
-  stop: function(event, ui) {
-    $("input#minCost").val(jQuery("#slider").slider("values",0));
-    $("input#maxCost").val(jQuery("#slider").slider("values",1));
+  stop: function (event, ui) {
+    $("input#minCost").val($("#slider").slider("values", 0));
+    $("input#maxCost").val($("#slider").slider("values", 1));
   },
-  slide: function(event, ui){
-    jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
-    jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+  slide: function (event, ui) {
+    $("input#minCost").val($("#slider").slider("values", 0));
+    $("input#maxCost").val($("#slider").slider("values", 1));
   }
 });
 
-jQuery("input#minCost").change(function(){
-  var value1=jQuery("input#minCost").val();
-  var value2=jQuery("input#maxCost").val();
+$("input#minCost").change(function () {
+  var value1 = $("input#minCost").val();
+  var value2 = $("input#maxCost").val();
 
-  if(parseInt(value1) > parseInt(value2)){
+  if (parseInt(value1) > parseInt(value2)) {
     value1 = value2;
-    jQuery("input#minCost").val(value1);
+    $("input#minCost").val(value1);
   }
-  jQuery("#slider").slider("values",0,value1);
+  $("#slider").slider("values", 0, value1);
 });
+$("input#maxCost").change(function () {
+  var value1 = $("input#minCost").val();
+  var value2 = $("input#maxCost").val();
 
+  if (value2 > 1000) {
+    value2 = 1000;
+    $("input#maxCost").val(1000)
+  }
 
-jQuery("input#maxCost").change(function(){
-  var value1=jQuery("input#minCost").val();
-  var value2=jQuery("input#maxCost").val();
-
-  if (value2 > 1000) { value2 = 1000; jQuery("input#maxCost").val(1000)}
-
-  if(parseInt(value1) > parseInt(value2)){
+  if (parseInt(value1) > parseInt(value2)) {
     value2 = value1;
-    jQuery("input#maxCost").val(value2);
+    $("input#maxCost").val(value2);
   }
-  jQuery("#slider").slider("values",1,value2);
+  $("#slider").slider("values", 1, value2);
 });
 
-/*$(document).ready(function() {
 
- $("#buy_price").slider({
- range: true,
- min: 500, // минимальное значение цены
- max: 120000, // максимальное значение цены
- step: 1500, // шаг слайдера
- values: [ 1500, 100000],  // начальные значения - позиции ползунков на шкале
 
- slide: function( event, ui ) {
- $( "input[name=price_s]" ).val(  ui.values[ 0 ] ); // выводим  значение от
- $( "input[name=price_f]" ).val(  ui.values[ 1 ] ); // выводим  значение до
- },
- stop: function(event, ui) { show(); } // выполняем действие  после остановки ползунка, в нашем случае функция show
- });
+/* slick menuNav  320px*/
 
- });
- */
+
 $(function () {
   $('#menu').slicknav({
     prependTo: menu
   });
 });
+
+/* slick slider */
+
 $(document).ready(function () {
   var $slider1 = $(".slider");
   $slider1.slick({
@@ -85,15 +80,14 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
     dots: true
   });
-
-  /*var $slider2 = $(".new-posts");
-  $(".nav-nev-post .prew").on("click", function (e) {
+  var $slider2 = $(".new-posts");
+  $(".nav-new-posts .prew").on("click", function (e) {
     e.preventDefault();
-    $slider2.slick("slick.prev");
+    $slider2.slick("slickPrev");
   });
-  $(".nav-nev-post .next").on("click", function (e) {
+  $(".nav-new-posts .next").on("click", function (e) {
     e.preventDefault();
-    $slider2.slick("slick.next");
+    $slider2.slick("slickNext");
   });
   $slider2.slick({
     arrows: false,
@@ -122,5 +116,8 @@ $(document).ready(function () {
         }
       }
     ]
-  });*/
+  });
+});
+$(function () {
+  $("select").uniform();
 });
