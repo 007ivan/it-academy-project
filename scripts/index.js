30e1206dad4127a8fe0390b89c711906ce14df85
Admin <ivan_kruk@ukr.net>
2016-12-08 16:13:01 +0200

    update  first  project 081216

--- scripts/index.js
+++ scripts/index.js
@@ -170,3 +170,26 @@ $(function () {
     }
   });
 });
+$( function() {
+  $( "#tabs" ).tabs();
+} );
+
+/* col tovaru */
+
+$(document).ready(function() {
+  $('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+});
+
